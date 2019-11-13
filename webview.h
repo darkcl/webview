e18456c5e2058f872f375242c519699994570da3
Yeung Yiu Hung <446404+darkcl@users.noreply.github.com>
2019-11-13 19:17:18 +0800

    fix(webview): remove unused include

--- webview.h
+++ webview.h
@@ -43,10 +43,6 @@ extern "C" {
 #include <gtk/gtk.h>
 #include <webkit2/webkit2.h>
 
-#if __APPLE__
-#include <objc/message.h>
-#endif
-
 struct webview_priv {
   GtkWidget *window;
   GtkWidget *scroller;
